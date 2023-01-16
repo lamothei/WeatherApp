584ae8926f683c75edb7ae3cfdb469a3cfa6acdd
Ian <jlmamothei@gmail.com>
2023-01-16 01:08:46 -0500

    finished up some remaining work, and functionality, tested function and works great for now.

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { UilSearch, UilMapMarkerInfo } from '@iconscout/react-unicons';
 
-function Inputs() {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState('');
+
+  const handleSearchClick = () => {
+    if (city !== '') setQuery({ q: city });
+  };
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({ lat, lon });
+      });
+    }
+  };
+
+  const handleUnitChange = (e) =>{
+    const selectedUnit = e.currentTarget.name
+    if (units !== selectedUnit) setUnits(selectedUnit);
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (city !== '') setQuery({ q: city });
+    }
+  };
+
   return (
     <div className="flex flex-row justify-center my-6">
       <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
         <input
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
           type="text"
           placeholder="Search for city...?"
           className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase"
+          onKeyDown={handleKeyDown}
         />
         <UilSearch
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleSearchClick}
         />
         <UilMapMarkerInfo
           size={25}
           className="text-white cursor-pointer transition ease-out hover:scale-125"
+          onClick={handleLocationClick}
         />
       </div>
       <div className="flex flex-row w-1/4 items-center justify-center">
-        <button name="metric" className=" text-xl text-white font-light transition ease-out hover:scale-125">
+        <button
+          name="metric"
+          className=" text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitChange}
+        >
           °C
         </button>
-        <p className='text-xl text-white mx-1'>|</p>
-        <button name="imperial" className=" text-xl text-white font-light transition ease-out hover:scale-125">
+        <p className="text-xl text-white mx-1">|</p>
+        <button
+          name="imperial"
+          className=" text-xl text-white font-light transition ease-out hover:scale-125"
+          onClick={handleUnitChange}
+        >
           °F
         </button>
       </div>
